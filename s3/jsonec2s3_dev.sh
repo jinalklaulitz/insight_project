96b8f0ccab3d150542b253dc08c324e93a2a0076
Marvin Mananghaya <mananghaya_marvin@yahoo.com>
2019-07-18 01:22:28 -0400

    uploading jaccardian similarity

--- s3/jsonec2s3_dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-while :
-    do
-        eval "aws s3 sync ~/twitchjson/. s3://$bucket"
-  	sleep 18000
-done
--- scrapper/upload_chatscrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {1..10}
-do
-    peg scp from-local spark-cluster $i /home/jinalklaulitz/Documents/Insight/chatscrapper.py .
-done
